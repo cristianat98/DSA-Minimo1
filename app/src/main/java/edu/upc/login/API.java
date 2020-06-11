f8e050b428196737693f6e5e37090b08ac2664a9
cristianat98 <cristianarmestotejedo@gmail.com>
2020-06-11 15:28:18 +0200

    Version Final

---
+++ app/src/main/java/edu/upc/login/API.java
@@ -0,0 +1,43 @@
+package edu.upc.login;
+
+import java.util.List;
+
+import edu.upc.login.Entidades.Ranking;
+import edu.upc.login.Entidades.Token;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface API {
+/*    @GET("tracks")
+    Call<List<User>> getTracks();
+
+    @POST("tracks")
+    Call<User> addTrack (@Body User user);
+
+    @PUT("tracks")
+    Call<Void> updateTrack (@Body User user);
+
+    @DELETE("tracks/{id}")
+    Call<Void> deleteTrack (@Path("id") String id);
+*/
+@GET("game/ranking")
+Call <RankingRespuesta> getRanking();
+
+//Servicio que pasa un JSON con las credenciales del login y devuelve un token
+//que se guardara en sharedPreferences
+//Para ver sharedPreferences: device file explorer->data->data->edu.upc.login->shared_prefs
+@POST("auth/login")
+    Call<Token> login(@Body LoginCredentials loginCredentials);
+
+@POST("auth/register")
+    Call<Token> register(@Body RegisterCredentials registerCredentials);
+
+@GET("user/user")
+    Call<User> getUser(@Query("token") String token);
+
+@POST("user/update")
+    Call<Void> updateUser(@Body User user);
+}
---
+++ app/src/main/java/edu/upc/login/User.java
@@ -0,0 +1,60 @@
+package edu.upc.login;
+
+public class User {
+
+    public String idUser;
+    public String nombre;
+    public String mail;
+    public String password;
+    public String status;
+
+    public User(){}
+
+    public User(String id, String nombre, String mail, String password, String status) {
+        this();
+        this.idUser = id;
+        this.nombre = nombre;
+        this.mail = mail;
+        this.password = password;
+        this.status = status;
+    }
+
+
+    public String getId() {
+        return idUser;
+    }
+
+    public void setId(String id) {
+        this.idUser = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id="+idUser+", name=" + nombre + ", mail=" + mail +"]";
+    }
+
+}
